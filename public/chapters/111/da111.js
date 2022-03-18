404c7ce4497f9cb559bd03dbb961cfb8762ea1a5
AHSarantaris <35047513+AHSarantaris@users.noreply.github.com>
2022-03-18 06:08:52 +0100

    update da111.js

--- public/chapters/111/da111.js
+++ public/chapters/111/da111.js
@@ -1,8 +1,8 @@
 s[111].da = {
     0: "Palmefiber",
     1: "Ruineret være Abu Lahabs hænder, og ruineret er han.",
-    2: "Ingen nytte for ham er hans rigdom og det han har bedrevet.",
-    3: "Han skal brænde i en ild .",
+    2: "Hans rigdom og det han tjente hnytter ham intet.",
+    3: "Han skal brænde i en ild fuld af flammer.",
     4: "Og hans hustru - bæreren af brænde.",
     5: "Om hendes hals er et reb af palmefiber."
 }
