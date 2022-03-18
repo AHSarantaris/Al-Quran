122aa6f8374a83b5067d30c4e77f02d90a71520d
AHSarantaris <35047513+AHSarantaris@users.noreply.github.com>
2022-03-18 06:06:33 +0100

    update en.js

--- public/chapters/111/en.js
+++ public/chapters/111/en.js
@@ -2,7 +2,7 @@
     "0": "Palm-fiber",
     "1": "Ruined be the hands of Abu Lahab, and ruined is he.",
     "2": "His wealth and what he earned avails him not.",
-    "3": "He shall burn in a fire [full] of flames.",
+    "3": "He shall burn in a fire full of flames.",
     "4": "And his wife - the carrier of firewood.",
     "5": "Around her neck is a rope of palm-fiber."
 }
