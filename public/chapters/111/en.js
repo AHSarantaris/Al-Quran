bad7508daf5b39ac2a17d9bf4bfaee3e37dbc31e
AHSarantaris <35047513+AHSarantaris@users.noreply.github.com>
2022-03-18 06:04:42 +0100

    update en.js

--- public/chapters/111/en.js
+++ public/chapters/111/en.js
@@ -3,6 +3,6 @@
     "1": "Ruined be the hands of Abu Lahab, and ruined is he.",
     "2": "His wealth and what he earned avails him not.",
     "3": "He shall burn in a flaming fire.",
-    "4": "And his wife - carrier of firewood.",
+    "4": "And his wife - the carrier of firewood.",
     "5": "Around her neck is a rope of palm-fiber."
 }
